7132e5e271579c1fa6e950443d8f00d908ffd2d6
benmartin688 <benmartin688@yahoo.co.nz>
2023-01-02 12:10:02 +1300

    refactor: removed unused intro component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { IntroComponent } from './intro/intro.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'intro', component: IntroComponent },
-  { path: '', redirectTo: '/intro', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { IntroComponent } from './intro/intro.component';
 import { NgParticlesModule } from 'ng-particles';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, IntroComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, NgParticlesModule],
   providers: [],
   bootstrap: [AppComponent],
